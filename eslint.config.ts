b936db87f32f169f2ef392ce59e3212132f95cd6
limitlesspc <48165199+limitlesspc@users.noreply.github.com>
2024-05-06 04:56:54 -0400

    merge antfu/eslint-config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,36 +1,15 @@
-// @ts-expect-error missing types
-import styleMigrate from '@stylistic/eslint-plugin-migrate'
-import { antfu } from './src'
 
-export default antfu(
+import { limitlesspc } from './src'
+
+export default limitlesspc(
   {
-    vue: true,
-    react: true,
-    solid: true,
     svelte: true,
-    astro: true,
     typescript: true,
-    formatters: true,
-  },
-  {
-    ignores: [
-      'fixtures',
-      '_fixtures',
-    ],
   },
   {
     files: ['src/**/*.ts'],
     rules: {
       'perfectionist/sort-objects': 'error',
-    },
-  },
-  {
-    files: ['src/configs/*.ts'],
-    plugins: {
-      'style-migrate': styleMigrate,
-    },
-    rules: {
-      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
     },
   },
 )
